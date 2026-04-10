728e45c7e09aefe8d12686b81e086965d82bf1af
Ataa17 <ataa.mars@yahoo.com>
2026-04-10 14:56:52 +0100

    defining routes

---
+++ config/routes.js
@@ -0,0 +1,8 @@
+export const routes = {
+    dashboard: { title: "Dashboard", path: "/" },
+    alerts: { title: "alerts", path: "/alerts" },
+    chat: { title: "chat", path: "/chat" },
+    metrics: { title: "metrics", path: "/metrics" },
+    signin : { title: "Sign In", path: "/auth/signin" },
+    signup : { title: "Sign Up", path: "/auth/signup" },
+}
